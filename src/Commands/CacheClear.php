a982fce24c815f00bd1bae280b2bcfdbefca3f8c
Lee Rowlands <lee.rowlands@previousnext.com.au>
2015-09-30 19:54:54 +1000

    Add tag single and cache clear

---
+++ src/Commands/CacheClear.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \Larowlan\Tl\Commands\CacheClear.php
+ */
+namespace Larowlan\Tl\Commands;
+
+use Doctrine\Common\Cache\FlushableCache;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Defines a class for clearing the cache.
+ */
+class CacheClear extends Command {
+
+  protected $cache;
+
+  public function __construct(FlushableCache $cache) {
+    $this->cache = $cache;
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('cache-clear')
+      ->setDescription('Clear your cached back-end data');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $this->cache->flushAll();
+    $output->writeln('<info>Caches cleared</info>');
+  }
+
+}
