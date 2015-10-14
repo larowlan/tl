aaa4c169759c3e38dadb5855ef891cdd4f864973
Lee Rowlands <lee.rowlands@previousnext.com.au>
2015-10-15 07:50:08 +1000

    Tests

--- tests/Commands/StartTest.php
+++ tests/Commands/StartTest.php
@@ -26,15 +26,7 @@ class StartTest extends TlTestBase {
       ->willReturn(['title' => 'Running tests']);
     $output = $this->executeCommand('start', ['issue_number' => 1234]);
     $this->assertRegExp('/Started new entry for 1234: Running tests/', $output->getDisplay());
-    /** @var Repository $repository */
-    $repository = $this->container->get('repository');
-    $active = $repository->getActive();
-    $this->assertEquals('1234', $active->tid);
-    $this->assertNull($active->comment);
-    $this->assertNull($active->end);
-    $this->assertNull($active->category);
-    $this->assertNull($active->teid);
-    return $active->id;
+    $this->assertTicketIsOpen(1234);
   }
 
   /**
@@ -49,21 +41,13 @@ class StartTest extends TlTestBase {
       ]);
     $output = $this->executeCommand('start', ['issue_number' => 1234]);
     $this->assertRegExp('/Started new entry for 1234: Running tests/', $output->getDisplay());
-    /** @var Repository $repository */
-    $repository = $this->container->get('repository');
-    $active = $repository->getActive();
+    $active = $this->assertTicketIsOpen(1234);
     $slot_id = $active->id;
     $output = $this->executeCommand('start', ['issue_number' => 4567]);
     $this->assertRegExp('/Closed slot [0-9]+ against ticket 1234/', $output->getDisplay());
     $this->assertRegExp('/Started new entry for 4567: Running more tests/', $output->getDisplay());
-
-    $active = $repository->getActive();
-    $this->assertEquals('4567', $active->tid);
-    $this->assertNull($active->comment);
-    $this->assertNull($active->end);
-    $this->assertNull($active->category);
-    $this->assertNull($active->teid);
-    $closed = $repository->slot($slot_id);
+    $this->assertTicketIsOpen('4567');
+    $closed = $this->getRepository()->slot($slot_id);
     $this->assertNotNull($closed->end);
     $this->assertEquals('1234', $closed->tid);
   }
@@ -81,16 +65,106 @@ class StartTest extends TlTestBase {
       'comment' => 'Doing stuff',
     ]);
     $this->assertRegExp('/Started new entry for 1234: Running tests/', $output->getDisplay());
+    $this->assertTicketIsOpen(1234, 'Doing stuff');
+  }
+
+  /**
+   * @covers ::execute
+   */
+  public function testAssign() {
+    $this->getMockConnector()->expects($this->any())
+      ->method('ticketDetails')
+      ->with(1234)
+      ->willReturn(['title' => 'Running tests']);
+    $this->getMockConnector()->expects($this->once())
+      ->method('assign')
+      ->with(1234)
+      ->willReturn(TRUE);
+    $output = $this->executeCommand('start', [
+      'issue_number' => 1234,
+      '--assign' => TRUE,
+    ]);
+    $this->assertRegExp('/Started new entry for 1234: Running tests/', $output->getDisplay());
+    $this->assertRegExp('/Ticket 1234 assigned to you/', $output->getDisplay());
+    $this->assertTicketIsOpen(1234);
+  }
+
+  /**
+   * @covers ::execute
+   */
+  public function testAssignShortSyntax() {
+    $this->getMockConnector()->expects($this->any())
+      ->method('ticketDetails')
+      ->with(1234)
+      ->willReturn(['title' => 'Running tests']);
+    $this->getMockConnector()->expects($this->once())
+      ->method('assign')
+      ->with(1234)
+      ->willReturn(TRUE);
+    $output = $this->executeCommand('start', [
+      'issue_number' => 1234,
+      '-a' => TRUE,
+    ]);
+    $this->assertRegExp('/Started new entry for 1234: Running tests/', $output->getDisplay());
+    $this->assertRegExp('/Ticket 1234 assigned to you/', $output->getDisplay());
+    $this->assertTicketIsOpen(1234);
+  }
+
+  /**
+   * @covers ::execute
+   */
+  public function testAssignAlreadyAssigned() {
+    $this->getMockConnector()->expects($this->any())
+      ->method('ticketDetails')
+      ->with(1234)
+      ->willReturn(['title' => 'Running tests']);
+    $this->getMockConnector()->expects($this->once())
+      ->method('assign')
+      ->with(1234)
+      ->willReturn(FALSE);
+    $output = $this->executeCommand('start', [
+      'issue_number' => 1234,
+      '--assign' => TRUE,
+    ]);
+    $this->assertRegExp('/Started new entry for 1234: Running tests/', $output->getDisplay());
+    $this->assertRegExp('/Ticket 1234 already assigned to you/', $output->getDisplay());
+    $this->assertTicketIsOpen(1234);
+  }
+
+  /**
+   * @covers ::execute
+   */
+  public function testStatus() {
+    $this->getMockConnector()->expects($this->any())
+      ->method('ticketDetails')
+      ->with(1234)
+      ->willReturn(['title' => 'Running tests']);
+    $this->getMockConnector()->expects($this->once())
+      ->method('setInProgress')
+      ->with(1234)
+      ->willReturn(TRUE);
+    $output = $this->executeCommand('start', [
+      'issue_number' => 1234,
+      '--status' => TRUE
+    ]);
+    $this->assertRegExp('/Started new entry for 1234: Running tests/', $output->getDisplay());
+    $this->assertRegExp('/Ticket 1234 set to in-progress/', $output->getDisplay());
+    $this->assertTicketIsOpen(1234);
+  }
+
+  /**
+   * @return mixed
+   */
+  protected function assertTicketIsOpen($ticket_id, $comment = NULL) {
     /** @var Repository $repository */
-    $repository = $this->container->get('repository');
+    $repository = $this->getRepository();
     $active = $repository->getActive();
-    $this->assertEquals('1234', $active->tid);
-    $this->assertEquals('Doing stuff', $active->comment);
+    $this->assertEquals($ticket_id, $active->tid);
+    $this->assertEquals($comment, $active->comment);
     $this->assertNull($active->end);
     $this->assertNull($active->category);
     $this->assertNull($active->teid);
-    return $active->id;
+    return $active;
   }
-
 
 }
