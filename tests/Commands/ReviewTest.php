a1c992a876e04ac22bad5fea42de2210a98ab407
Lee Rowlands <lee.rowlands@previousnext.com.au>
2015-12-22 13:59:29 +1000

    [#50] Test for --exact option on Review command

--- tests/Commands/ReviewTest.php
+++ tests/Commands/ReviewTest.php
@@ -16,9 +16,10 @@ use Larowlan\Tl\Ticket;
 class ReviewTest extends TlTestBase {
 
   /**
-   * Test the basic functionality of the review command.
+   * {@inheritdoc}
    */
-  public function testReviewCommand() {
+  protected function setUp() {
+    parent::setUp();
     $this->getMockConnector()->expects($this->any())
       ->method('ticketDetails')
       ->willReturnMap([
@@ -30,15 +31,49 @@ class ReviewTest extends TlTestBase {
     // Five entries for today.
     $start = time();
     // 7 hrs.
-    $repository->insert(['tid' => 1, 'start' => $start, 'end' => $start + 3588 * 7]);
+    $repository->insert([
+      'tid' => 1,
+      'start' => $start,
+      'end' => $start + 3588 * 7
+    ]);
     // 1 hr.
-    $repository->insert(['tid' => 2, 'start' => $start, 'end' => $start + 3600]);
+    $repository->insert([
+      'tid' => 2,
+      'start' => $start,
+      'end' => $start + 3600
+    ]);
     // 3 hrs.
-    $repository->insert(['tid' => 3, 'start' => $start, 'end' => $start + 3600 * 3]);
+    $repository->insert([
+      'tid' => 3,
+      'start' => $start,
+      'end' => $start + 3600 * 3
+    ]);
+  }
+
+  /**
+   * Test the basic functionality of the review command.
+   */
+  public function testReviewCommand() {
+    $this->setUp();
     $result = $this->executeCommand('review');
     $this->assertContains('11 h', $result->getDisplay());
     $this->assertContains('Do something', $result->getDisplay());
     $this->assertContains('Do something else', $result->getDisplay());
     $this->assertContains('Do something more', $result->getDisplay());
   }
+
+  /**
+   * Test the basic functionality of the review command.
+   */
+  public function testReviewExact() {
+    $this->setUp();
+    $result = $this->executeCommand('review' ,['--exact' => TRUE]);
+    $this->assertContains('10:58:36', $result->getDisplay());
+    $this->assertContains('6:58:36', $result->getDisplay());
+    $this->assertContains('3:00:00', $result->getDisplay());
+    $this->assertContains('Do something', $result->getDisplay());
+    $this->assertContains('Do something else', $result->getDisplay());
+    $this->assertContains('Do something more', $result->getDisplay());
+  }
+
 }
