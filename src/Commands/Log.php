2b342013e3b9ad745759711f6b7916a6d527091e
Lee Rowlands <lee.rowlands@previousnext.com.au>
2015-12-22 10:28:53 +1000

    [#59] Adds log command

---
+++ src/Commands/Log.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @file
+ * Contains \Larowlan\Tl\Commands\Log.php
+ */
+
+namespace Larowlan\Tl\Commands;
+
+use Larowlan\Tl\LogHelper;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Log extends Command {
+
+  /**
+   * Installed directory.
+   *
+   * @var string
+   */
+  protected $directory;
+
+  public function __construct($directory) {
+    $this->directory = $directory;
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('log')
+      ->setDescription('Shows the log entries')
+      ->setHelp('Shows log entries. <comment>Usage:</comment> <info>tl log [records]</info>')
+      ->addArgument('records', InputArgument::OPTIONAL, 'Number of records', 10)
+      ->addUsage('tl log 100');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $records = $input->getArgument('records');
+    $output->writeln(LogHelper::tail($this->directory . '/.tl.log', $records));
+  }
+
+}
---
+++ src/LogHelper.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @file
+ * Contains LogHelper.php
+ */
+
+namespace Larowlan\Tl;
+
+/**
+ * Defines a class for getting the tail of a log file.
+ */
+class LogHelper {
+
+  /**
+   * Tails a file.
+   *
+   * @param string $filepath
+   *   File to read from
+   * @param int $lines
+   *   Number ofl lines
+   * @param bool|TRUE $adaptive
+   *   Adapt the buffer size.
+   *
+   * @return bool|string
+   *   Output as appropriate.
+   *
+   * Adapted from https://gist.github.com/lorenzos/1711e81a9162320fde20
+   */
+  public static function tail($filepath, $lines = 1, $adaptive = true) {
+
+    // Open file.
+    $f = @fopen($filepath, "rb");
+    if ($f === FALSE) {
+      return FALSE;
+    }
+
+    // Set buffer size.
+    if (!$adaptive)  {
+      $buffer = 4096;
+    }
+    else {
+      $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+    }
+
+    // Jump to last character.
+    fseek($f, -1, SEEK_END);
+
+    // Read it and adjust line number if necessary. Otherwise the result would
+    // be wrong if file doesn't end with a blank line.
+    if (fread($f, 1) !== "\n")  {
+      $lines -= 1;
+    }
+
+    // Start reading.
+    $output = '';
+
+    // While we would like more.
+    while (ftell($f) > 0 && $lines >= 0) {
+      // Figure out how far back we should jump.
+      $seek = min(ftell($f), $buffer);
+      // Do the jump (backwards, relative to where we are).
+      fseek($f, -$seek, SEEK_CUR);
+      // Read a chunk and prepend it to our output.
+      $output = ($chunk = fread($f, $seek)) . $output;
+      // Jump back to where we started reading.
+      fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+      // Decrease our line counter.
+      $lines -= substr_count($chunk, "\n");
+    }
+
+    // While we have too many lines:
+    // (Because of buffer size we might have read too many).
+    while ($lines++ < 0) {
+      // Find first newline and remove all text before that.
+      $output = substr($output, strpos($output, "\n") + 1);
+    }
+
+    // Close file and return
+    fclose($f);
+    return trim($output);
+  }
+
+}
