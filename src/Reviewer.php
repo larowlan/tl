aa226612238ab8308ae9774b139ebd5fa5a72976
Sam <sam@1485048.no-reply.drupal.org>
2017-05-29 17:00:19 +0800

    Add formatting to review command.

--- src/Reviewer.php
+++ src/Reviewer.php
@@ -77,10 +77,11 @@ class Reviewer {
           $category = 'Unknown';
         }
       }
+      $duration = sprintf('<fg=%s>%s</>', $details->isBillable() ? 'default' : 'yellow', $record->duration);
       $row = [
         $record->id,
         $record->tid,
-        $record->duration,
+        $duration,
       ];
       if ($exact) {
         $row[] = Formatter::formatDuration($record->end - $record->start);
