4155a420893dd8e94cc690a07de9ee9c91d98279
Nicholas Fletcher <nick.fletcher@previousnext.com.au>
2020-05-15 16:16:29 +1000

    [#128] Fix #128 by adding the visit link, do not show if inactive.

--- tl.15s.sh
+++ tl.15s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Put this in your bitbar plugins folder
+# Put this in your bitbar plugins folder 🥃
 # Make sure it is executable
 # Make sure tl is available in your path from /usr/local/bin or /usr/bin
 export PATH='/usr/local/bin:/usr/bin:$PATH'
@@ -7,6 +7,10 @@ export LANG="${LANG:-en_US.UTF-8}"
 if [[ "$1" = "stop" ]]; then
   output=`tl stop`
   osascript -e "display notification \"$output\" with title \"Stopped timer\"" &> /dev/null
+fi
+if [[ "$1" = "visit" ]]; then
+  output=`tl visit`
+  osascript -e "display notification \"$output\" with title \"Opening ticket\"" &> /dev/null
 fi
 OUTPUT="$(tl bitbar)"
 COLOR="green"
@@ -20,3 +24,6 @@ echo "${ICON} ${OUTPUT} | color=${COLOR}"
 echo "---"
 echo "Stop|bash=$0 param1=stop terminal=false refresh=true"
 echo "Refresh|refresh=true"
+if [[ ${OUTPUT:0:8} != "Inactive" ]]; then
+  echo "Visit Task|bash=$0 param1=visit terminal=false"
+fi
